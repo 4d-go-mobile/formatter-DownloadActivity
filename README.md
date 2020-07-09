0e5151c0d5d2456b8503772b5e9cdd6c67c1fafb
Eric Marchand <eric.marchand@4d.com>
2020-07-09 15:34:51 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center"><img src="https://github.com/4d-for-ios/4d-for-ios-formatter-Download/blob/master/formatter.png" alt=“Url” height="auto" width="200"></p>
+
+## Download Activity
+
+* **Format:** Text ⟶ URL
+* **Function:** download the file from URL and open activity to save it
+* **Type:** Swift formatter
+
+## How to integrate
+
+* To use a custom formatter, the first thing you'll need to do is create a YourDatabase.4dbase/Resources/formatters folder.
+* Then drop the formatter folder into it.
---
+++ Sources/Formatters/UILabel+downloadActivity.swift
@@ -0,0 +1,47 @@
+//
+//  UILabel+download.swift
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//  ___COPYRIGHT___
+
+import UIKit
+import QMobileUI
+import Alamofire
+import XCGLogger
+
+extension UILabel {
+
+    @objc dynamic public var downloadActivityAction: String? {
+        get {
+            return self.text
+        }
+        set {
+            self.text = newValue
+            if let newValue = newValue, URL(string: newValue) != nil {
+                let tap = UITapGestureRecognizer(target: self, action: #selector(downloadActivityActionTap(_:)))
+                self.isUserInteractionEnabled = true
+                self.addGestureRecognizer(tap)
+            } else {
+                for gesture in self.gestureRecognizers ?? [] {
+                    self.removeGestureRecognizer(gesture)
+                }
+                self.isUserInteractionEnabled = false
+            }
+        }
+    }
+
+    @objc func downloadActivityActionTap(_ sender: Any) {
+        guard let urlString = self.text, let url = URL(string: urlString) else {
+            return
+        }
+        AF.request(url).responseData { response in
+            if let fileData = response.data {
+                let activityViewController = UIActivityViewController(activityItems: [url.lastPathComponent, fileData], applicationActivities: nil)
+                UIApplication.topViewController?.present(activityViewController, animated: true) {
+                    logger.info("End to download \(url)")
+                }
+            }
+        }
+    }
+}
